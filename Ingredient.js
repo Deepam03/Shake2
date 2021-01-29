ff1dde13d40adf0f163a3bafc716a388088c804a
Deepam03 <76608955+Deepam03@users.noreply.github.com>
2021-01-29 17:14:36 +0530

    Add files via upload

---
+++ Ingredient.js
@@ -0,0 +1,5 @@
+Ingridients :-
+bananas
+coconut milk
+vanilla powder
+cardamom.
---
+++ Recipe.js
@@ -0,0 +1,4 @@
+1. Peel and chop 3 medium sized bananas. Add the chopped bananas in a mixer or blender jar.
+2. Then add 2 cups thin coconut milk.
+3. Mix and add 2 spoon caradamom
+4. shake's ready.
